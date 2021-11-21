959b3df40e244921503f536e1b6e2ebaa242db62
Morgan Rosenberg <morgan.ar.rosenberg@gmail.com>
2021-11-21 12:45:44 -0800

    Adding Exercise 5 adaptation in demo.R

--- demo.R
+++ demo.R
@@ -8,9 +8,11 @@ Options:
 <arg>             Takes any value (this is a required positional argument)
 --arg2=<arg2>     Takes any value (this is a required option)
 [--arg3=<arg3>]   Takes any value (this is an optional option)
+[--arg4=<arg4>]   Takes any value (this is an optional option)
 " -> doc
 
 library(docopt)
 opt <- docopt(doc)
+if (!is.null(arg4)) {print(arg4)}
 print(opt)
 print(typeof(opt))
